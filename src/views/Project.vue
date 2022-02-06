99ac1694f5e418e17964761c069d11eb9be7e753
You Song Hou <yousonghou@gmail.com>
2022-02-06 01:58:59 -0500

    feat: add Vuetify tabs

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -19,37 +19,74 @@
       </v-card-actions>
     </v-card>
 
-    <v-textarea
-      solo
-      class="mb-0"
-      rows="4"
-      label="Add comment"
-      v-model="comment"
-    ></v-textarea>
-    <v-btn class="btn-add" @click="addComment()">
-      Add Comment
-    </v-btn>
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        background-color="primary"
+        dark
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item.tab"
+        >
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
 
-    <div v-bind:key="comment.id" v-for="comment of comments">
-      <Comment :comment="comment"/>
-    </div>
+      <v-tabs-items v-model="tab">
+        <v-tab-item key="Announcements">
+          <h1>Announcements</h1>
+        </v-tab-item>
+        <v-tab-item key="Comments">
+          <h1>Comments</h1>
+          <v-textarea
+            solo
+            class="mb-0"
+            rows="4"
+            label="Add comment"
+            v-model="comment"
+          ></v-textarea>
+          <v-btn class="btn-add" @click="addComment()">
+            Add Comment
+          </v-btn>
+
+          <div v-bind:key="comment.id" v-for="comment of comments">
+            <Comment :comment="comment"/>
+          </div>
+        </v-tab-item>
+         <v-tab-item key="Mint">
+          <h1>Mint</h1>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+
+    
    
   </v-container>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
-  import firebase from 'firebase';
+  import firebase from 'firebase'
 
-  import fb from '../config/firebase';
+  import fb from '../config/firebase'
   import Comment from '../components/Comment.vue'
-;
+
   export default {
     name: 'Project',
+    components: {
+      Comment
+    },
     data: () => ({
       project: {},
       comments: [],
-      comment: ""
+      comment: "",
+      tab: null,
+      items: [
+        { tab: 'Announcements'},
+        { tab: 'Comments'},
+        { tab: 'Mint' }
+      ],
     }),
     components: {
       Comment
