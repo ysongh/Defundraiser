446f2ec3885a09740df8d8b8a991c19ab6d027e3
You Song Hou <yousonghou@gmail.com>
2022-02-06 17:04:14 -0500

    feat: add donate, claim, and revoke fund features for SocialFundraiser contract

--- contracts/SocialFundraiser.sol
+++ contracts/SocialFundraiser.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-contract SocialFundraiser {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SocialFundraiser is ERC721  {
   uint public projectCount = 0;
   mapping(uint => Project) public projects;
   mapping(uint => DonationNFT) public donationNFTs;
 
-  constructor() public {
+  constructor() public  ERC721("Social Fundraiser Receipt", "SFR") {
   }
 
   struct Project {
@@ -49,5 +51,42 @@ contract SocialFundraiser {
 
     projects[projectCount] = Project(projectCount, _title, _description, 0, 0, new uint[](0), msg.sender);
     emit ProjectCreated(projectCount, _title, _description, 0, 0, new uint[](0), msg.sender);
+  }
+
+  function donateToProject(uint _projectId, uint _duration) payable public  {
+    uint _tokenId = totalSupply().add(1);
+    _safeMint(msg.sender, _tokenId);
+
+    uint _claimDate = block.timestamp + _duration;
+
+    donationNFTs[_tokenId] = DonationNFT(_tokenId, _projectId, _claimDate, msg.value, msg.sender);
+
+    Project storage _project = projects[_projectId];
+    _project.donationAmount +=  msg.value;
+    _project.listLenght += 1;
+    _project.donationIds.push(_tokenId);
+
+    emit Donation(_tokenId, _claimDate, msg.value, msg.sender);
+  }
+
+  function claimFundFromFunder(uint _projectId, uint _nftId) public {
+    Project storage _project = projects[_projectId];
+    DonationNFT storage _donationNFT = donationNFTs[_nftId];
+    require(block.timestamp > _donationNFT.claimDate, "You need to wait to claim fund");
+
+    msg.sender.transfer(_donationNFT.amount);
+    _project.donationAmount -= _donationNFT.amount;
+    _donationNFT.amount = 0;
+  }
+
+  function revokeFundFromProject(uint _projectId, uint _nftId) public {
+    require(ownerOf(_nftId) == msg.sender, "You do not own this NFT");
+
+    Project storage _project = projects[_projectId];
+    DonationNFT storage _donationNFT = donationNFTs[_nftId];
+
+    msg.sender.transfer(_donationNFT.amount);
+    _project.donationAmount -= _donationNFT.amount;
+    _donationNFT.amount = 0;
   }
 }
