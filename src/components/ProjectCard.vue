de9eb908b372ebb86895929f104bc29e2ac35533
You Song Hou <yousonghou@gmail.com>
2022-02-05 23:02:00 -0500

    feat: display project list

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    class="mb-2"
+  >
+    <v-card-text class="font-weight-bold">
+      <h2>{{ project.title}}</h2>
+      <p>{{ project.description}}</p>
+    </v-card-text>
+    <v-card-actions class="d-flex justify-space-between align-center">
+      <v-btn
+        color="orange"
+        text
+        @click="goToProjectpage()"
+      >
+        View
+      </v-btn>  
+      <p class="mt-3">{{ project.donationAmount}} MATIC Donated</p>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: {
+    project: Object
+  },
+  methods: {
+    goToProjectpage() {
+      this.$router.push('/project');
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,47 @@
       Add Project
     </router-link>
      
+    <div v-bind:key="project.id" v-for="project of projects">
+      <ProjectCard :project="project"/>
+    </div>
   </v-container>
 </template>
 
 <script>
+  import ProjectCard from '../components/ProjectCard.vue'
+  import { mapGetters } from 'vuex'
 
   export default {
     name: 'Home',
-
+    data: () => ({
+      projects: [],
+    }),
     components: {
-      
+      ProjectCard
     },
+    computed: mapGetters(['socialFundraiserBlockchain', 'projectCount']),
+    watch: {
+      projectCount: async function () {
+        let _projects = []
+
+        for(let i = 0; i < this.projectCount; i++){
+            const project = await this.socialFundraiserBlockchain.methods.projects(i + 1).call()
+            _projects.push(project)
+        }
+
+        this.projects = _projects
+        console.log(this.projects)
+      }
+    },
+    async created() {
+      let _projects = []
+
+      for(let i = 0; i < this.projectCount; i++){
+          const project = await this.socialFundraiserBlockchain.methods.projects(i + 1).call()
+          _projects.push(project)
+      }
+
+      this.projects = _projects
+    }
   }
 </script>
